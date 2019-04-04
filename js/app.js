7fa18ca16edb1b75aa1b822d6cb1c38850a42501
Brian Thomas <bthomas@imaginuity.com>
2019-04-04 15:47:08 -0500

    inital commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var sliders = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < sliders.length; i++) {
+    sliders[i].className = sliders[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  sliders[slideIndex - 1].className += " active";
+}
